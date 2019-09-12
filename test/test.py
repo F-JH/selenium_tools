45d8af3632376fdd670884da10f3230a717d2930
F-JH <gaodaziyou@126.com>
2019-09-12 17:13:29 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,27 +1,17 @@
 import os
 import sys
-import time
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from tools import Connect
 from tools import Debug
-from selenium import webdriver as wd
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 webdriver = Connect.webdriver()
-
-def check(driver):
-    if os.system('tasklist | findstr chromedriver.exe'):
-        return False
-    return True
-
 
 if __name__ == '__main__':
     dr = webdriver.Chrome()
-    Debug.start(dr)
+    # Debug.start(dr)
     dr.get("http://www.baidu.com")
     kw = dr.find_element_by_id("kw")
     su = dr.find_element_by_id("su")
-    su.send_keys("Selenium学习")
-    kw.click()
+    # kw.send_keys("Selenium学习")
+    kw.send_keys("Selenium重新使用已打开的浏览器实例")
+    su.click()
     # dr.quit()
